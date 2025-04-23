9f0363f1cfddf6984cd4449186604e23bfe84319
Maurice <mau.topf@gmail.com>
2025-04-23 12:56:20 +0200

    remove documentation

--- main.py
+++ main.py
@@ -2,21 +2,6 @@ import requests
 
 
 def get_ip_info(ip):
-    """
-    Retrieves detailed information about an IP address by making a request
-    to the IP-API service. The returned data includes details such as
-    location, ISP, and other metadata provided by the API.
-
-    :param ip: The IP address to retrieve information for. Must be a valid
-        string representation of an IPv4 or IPv6 address.
-    :type ip: str
-    :return: A dictionary containing the JSON response from the IP-API
-        service. This includes keys such as 'status', 'country', 'region',
-        and more metadata about the specified IP address.
-    :rtype: dict
-    :raises requests.exceptions.RequestException: If the HTTP request
-        fails during communication with the API.
-    """
     url = f"http://ip-api.com/json/{ip}?fields=66846719"
     response = requests.get(url)
     return response.json()
